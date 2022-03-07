ab90cbb586e9d1270e75fae4c641e76d2fb12711
Samson Achiaga <sirhumble84@gmail.com>
2022-03-07 04:08:29 +0100

    Updated the ResultFragment.kt to display recommendations to the user based on their symptoms

--- app/src/main/java/com/certified/covid19response/data/model/Result.kt
+++ app/src/main/java/com/certified/covid19response/data/model/Result.kt
@@ -26,11 +26,11 @@ data class Result(
             noOfSevereSymptoms >= 1 -> {
                 "Seek immediate medical attention if you have serious symptoms. Always call before visiting your doctor or health facility."
             }
-            lessPercent >= 1 -> {
-                "People w"
+            mostPercent > lessPercent -> {
+                "People with mild  symptoms who are otherwise healthy should manage their symptoms at home."
             }
-            mostPercent >= 1 -> {
-                ""
+            lessPercent > mostPercent -> {
+                "On average it takes 5 - 6 days from when someone is infected with the virus for symptoms to show, however it can take up to 14 days."
             }
             else -> {
                 "E be like say your own don finish o. As a matter of fact, death is lurking around you. You no fit survive boss."
